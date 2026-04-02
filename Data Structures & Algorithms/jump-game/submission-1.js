c63538bcf79583107ff8be7de1f11a7b1da83505
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-02 12:27:57 -0400

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        let n = nums.length;
+        let current = n - 2;
+        let goal = n - 1;
+        while(current>=0){
+            if(current + nums[current]===goal || current+nums[current]>goal){
+                goal = current;
+            }
+            current--;
+        }
+       return goal===0 ? true:false;
+    }
+}
