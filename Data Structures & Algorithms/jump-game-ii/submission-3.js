bb4f27c4454dcde785f0cad82796f2cea165e602
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-03 09:45:42 -0400

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        let cache = new Map();
+        function dfs(index){
+            if(index>=nums.length-1) return 0;
+            let min = Infinity;
+            if(cache.has(index)) return cache.get(index);
+            if(nums[index]===0) return Infinity;
+         for(let k=nums[index];k>=1;k--){
+                    let sum = 1 + dfs(index + k);
+                   min = Math.min(sum,min);
+                }
+            cache.set(index, min);
+            return min;
+
+        }
+        return dfs(0);
+    }
+}
