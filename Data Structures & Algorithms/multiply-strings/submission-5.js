2838c8bd5cee99be8e648a877aab2bb54c20651f
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-09 11:01:37 -0400

    Add: multiply-strings - submission-5

---
+++ Data Structures & Algorithms/multiply-strings/submission-5.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} num1
+     * @param {string} num2
+     * @return {string}
+     */
+    reverse(str){
+        let x = "";
+        for(let i=str.length - 1;i>=0;i--){
+         x+=str[i];
+        }
+        return x;
+    }
+    multiply(num1, num2) {
+     if(num1 ==="0" || num2 ==="0") return "0"
+     let result = Array(num1.length + num2.length).fill(0);
+     let number1 = this.reverse(num1);
+     let number2 = this.reverse(num2);
+     for(let i=0;i<number1.length;i++){
+        for(let j=0;j<number2.length;j++){
+            let sum = result[i+j] + number1[i] * number2[j];
+            result[i+j] = sum  % 10;
+            result[i+j+1] += Math.floor(sum / 10)
+        }
+     }
+     let res = result.reverse().join("");
+     let i = 0;
+     while(i < res.length && res[i]==="0"){
+        i++;
+     }
+     console.log(res);
+     return res.slice(i);
+    }
+}
