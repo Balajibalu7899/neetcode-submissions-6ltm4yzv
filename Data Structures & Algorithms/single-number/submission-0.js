8a7f6f49c5c7975a25e23af8245cd83327c95bc5
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-10 14:20:10 -0400

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let set = new Set();
+        for(let num of nums){
+            if(set.has(num)){
+                set.delete(num);
+            }else{
+                set.add(num);
+            }
+        }
+       for(let val of set){
+        return val;
+       }
+    }
+}
