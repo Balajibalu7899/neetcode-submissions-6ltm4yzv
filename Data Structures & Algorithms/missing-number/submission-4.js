2df0df827f7d8d79c77362f9ebab0d79c7790966
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-11 12:12:04 -0400

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        let hashset = new Set();
+        let max = 0;
+        for(let i=0;i<nums.length;i++){
+            hashset.add(nums[i]);
+            max = Math.max(nums[i], max);
+        }
+        console.log(hashset);
+        for(let i=0;i<=max;i++){
+            if(!hashset.has(i)) {
+                return i;
+            };
+        }
+        return max+1;
+    }
+}
