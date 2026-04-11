b57ae851909942466c1d85ed134275fc0fbbbb02
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-11 15:32:42 -0400

    Add: sum-of-two-integers - submission-3

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} a
+     * @param {number} b
+     * @return {number}
+     */
+    getSum(a, b) {
+        let res = 0;
+        let carry = 0;
+        for(let i=0;i<32;i++){
+            let b1 = ((a >> i) & 1);
+            let b2 = ((b >> i) & 1);
+            let l1 = b1  ^ b2 ^ carry;
+            carry = (b1 & b2) | (b1 & carry) | (b2 & carry)
+            res |= (l1 << i);
+        }
+        return res
+    }
+}
