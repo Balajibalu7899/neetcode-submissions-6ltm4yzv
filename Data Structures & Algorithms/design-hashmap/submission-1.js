9250ccb7519c919a9194d43f613229c0cfb1805b
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-14 09:42:32 -0400

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.js
@@ -0,0 +1,64 @@
+class MyHashMap {
+    constructor(size=7) {
+        this.array = new Array(size);
+    }
+    _hash(k){
+        let key = String(k);
+        let hash = 0;
+        for(let i=0;i<key.length;i++){
+            hash = ( hash + key.charCodeAt(i) * 23) % this.array.length;
+        }
+        return hash;
+    }
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        let index = this._hash(key);
+        if(!this.array[index]) this.array[index] = [];
+        for(let i=0;i<this.array[index].length;i++){
+            if(this.array[index][i][0]===key){
+                this.array[index][i][1] = value;
+                return;
+            }else{
+                continue;
+            }
+        }
+        this.array[index].push([key, value]);
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        let index = this._hash(key);
+        if(this.array[index]){
+            for(let [hashkey, value] of this.array[index]){
+                if(hashkey===key) return parseInt(value);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        let index = this._hash(key);
+        if(this.array[index]){
+            this.array[index] = this.array[index].filter((ele)=>ele[0]!==key);
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
