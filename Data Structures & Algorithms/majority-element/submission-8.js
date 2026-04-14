50b7158ddcbaf5e3c9484fed0ad67566b87f0994
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2026-04-14 08:54:52 -0400

    Add: majority-element - submission-8

---
+++ Data Structures & Algorithms/majority-element/submission-8.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let count = 0;
+        let res = 0;
+        for(let num of nums){
+            if(count===0) res = num;
+            if(num===res){
+                count++;
+            }else{
+                count--;
+            }
+            
+        }
+        return res;
+    }
+}
